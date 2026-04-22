8babe7f37c5833a44a8da879b246da70c30c8237
Dhanya-Krishna <dhanyaklg@gmail.com>
2026-04-22 11:09:15 +0530

    Add getProductName method to Product class
    Added getProductName method to retrieve product name.

--- Product.java
+++ Product.java
@@ -1,38 +1,36 @@
+package oopproject;
 public class Product {
-
         private int productId;
         private String productName;
         private double price;
         private double tax;
-
         public Product() {}
-
         public Product(int id, String name, double price, double tax) {
             this.productId = id;
             this.productName = name;
             this.price = price;
             this.tax = tax;
         }
-
         public double getSellingPrice() {
             return price + (price * tax / 100);
         }
-
         public double getPrice() {
             return price;
         }
-
         public void setPrice(double price) {
             this.price = price;
         }
-
         public int getProductId() {
             return productId;
         }
-
         @Override
         public String toString() {
             return productName + " (ID: " + productId + ")";
         }
+        public String getProductName() {
+            return productName;
+        }
     }
 
+
+        
