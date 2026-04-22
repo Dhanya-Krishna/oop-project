82ae2b1a002599ca78673c10abb3a2337cd872b3
Dhanya-Krishna <dhanyaklg@gmail.com>
2026-04-22 11:10:32 +0530

    Refactor Supplier class methods
    Updated the calculateDiscount method to accept an amount parameter and added null check in addProduct method.

--- Supplier.java
+++ Supplier.java
@@ -1,30 +1,41 @@
-
-    import java.util.ArrayList;
+package oopproject;
+import java.util.ArrayList;
 import java.util.List;
-
-    public class Supplier {
+public class Supplier {
         private int supplierId;
         private String supplierName;
         private double discountRate;
         private List<Product> suppliedProducts;
-
         public Supplier() {
             suppliedProducts = new ArrayList<>();
         }
-
         public Supplier(int id, String name, double discount) {
             this.supplierId = id;
             this.supplierName = name;
             this.discountRate = discount;
             this.suppliedProducts = new ArrayList<>();
         }
-
-        public double calculateDiscount() {
-            return discountRate;
+        public double calculateDiscount(double amount) {
+        	 return amount * discountRate / 100;
         }
-
         public void addProduct(Product p) {
+        	if (p == null) {
+                System.out.println("Invalid product.");
+                return;
+            }
             suppliedProducts.add(p);
         }
+        public int getSupplierId() {
+            return supplierId;
+        }
+
+        public String getSupplierName() {
+            return supplierName;
+        }
+
+        public List<Product> getSuppliedProducts() {
+            return suppliedProducts;
+        }
     }
+
 
